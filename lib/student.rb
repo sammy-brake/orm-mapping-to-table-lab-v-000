1fd1d0d42759635103e6f740bd1de5493efcec3d
Samantha brake <sammy.brake@gmail.com>
2018-11-09 02:11:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 def initialize (name, grade)
-  @name = name 
-  @grade = grade 
+  @name = name
+  @grade = grade
 end
 end
