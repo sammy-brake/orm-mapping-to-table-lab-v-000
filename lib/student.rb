9d59bb1a6dae658b2db1430fbcfc8bf9ca65845f
Samantha brake <sammy.brake@gmail.com>
2018-11-09 02:21:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,9 +19,9 @@ class Student
     end
 
   def self.drop_table
-    sql = <<-SQL 
-    DROP TABLE students 
-    SQL 
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
     DB[:conn].execute(sql)
   end
 
