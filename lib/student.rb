8b8ad089bd088b2df0f478f49c28d701b1705660
Samantha brake <sammy.brake@gmail.com>
2018-11-09 02:46:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   def initialize (name, grade, id= nil)
     @name = name
     @grade = grade
-    
+
   end
 
   def self.create_table
@@ -32,6 +32,8 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 
 end
